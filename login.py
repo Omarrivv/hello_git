1270b79f27af73036156daa6a92fc5f7fada4169
Omarrivv <omar.rivera@vallegrande.edu.pe>
2025-06-02 17:09:07 -0500

    Autenticacion ADIN

--- login.py
+++ login.py
@@ -1,1 +1,17 @@
-print("Login V2")
+print("Login V2 Modificado Arreglado")
+
+username = input("### Enter your username: ")
+password = input("### Enter your password: ")
+
+usernameAdmin = "admin"
+passwordAdmin = "admin123"
+
+if username == usernameAdmin and password == passwordAdmin:
+    print("Welcome, Admin!")
+elif username and password:
+    print(f"Welcome, {username}!")
+else:
+    print("Please enter both username and password.") # a español dice "por favor ingrese ambos nombres de usuario y contraseña."
+
+# This code prompts the user for a username and password, and then greets them if both are provided.
+# If either is missing, it asks them to enter both.
